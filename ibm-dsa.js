e5e6e2e30cddb46d052848ac4fcce3f535cf4fc4
abhishek5677 <abhishekgarad5677@gmail.com>
2026-01-12 00:53:28 +0530

    first character that appears twice in a string

--- ibm-dsa.js
+++ ibm-dsa.js
@@ -1,16 +1,34 @@
 // reverse a string
 
-const input = "abhishek";
+// const input = "abhishek";
 
-const reverseStr = (n) => {
-  let tempStr = "";
+// const reverseStr = (n) => {
+//   let tempStr = "";
 
-  for (let i = n.length - 1; i >= 0; i--) {
-    tempStr = tempStr + n[i];
-  }
-  return tempStr;
-};
+//   for (let i = n.length - 1; i >= 0; i--) {
+//     tempStr = tempStr + n[i];
+//   }
+//   return tempStr;
+// };
 
-const output = reverseStr(input);
+// const output = reverseStr(input);
 
-console.log(output);
+// console.log(output);
+
+
+// “Find the first character that appears twice in a string”?
+
+// const input = "abhishek";
+
+// const firstRepeatedChar = (str) => {
+//   let freq = {};
+
+//   for (const char of str) {
+//     if (freq[char]) {
+//       return char;
+//     }
+//     freq[char] = 1;
+//   }
+// };
+
+// console.log(firstRepeatedChar(input));
